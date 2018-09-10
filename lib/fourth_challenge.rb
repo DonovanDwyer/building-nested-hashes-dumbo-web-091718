cc63815c10b9059782b5b54077f2be994f3134cf
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-10 18:54:20 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
           {name: "Benvolio", age: "17", attitude: "worried"},
-          {name: "Mercutio", age: "18", attitude: "hot-headed"},
+          {name: "Steven", age: "30", attitude: "confused"}
       ]
    }, 
    :capulet => {
@@ -15,7 +15,8 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {name: "Nurse", age: "44", attitude: "worried"
-        {name: "Steven", age: "30", attitude: "confused"}
+        {name: "Mercutio", age: "18", attitude: "hot-headed"},
+        
       ]
    }
 }
